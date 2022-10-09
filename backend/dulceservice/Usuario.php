0c42b1dddefd347124af88f373b5977869f5bd9b
Eyder Suarez <eyder.suarez@correounivalle.edu.co>
2022-10-09 11:31:54 -0500

    backend funcional

--- backend/dulceservice/Usuario.php
+++ backend/dulceservice/Usuario.php
@@ -16,7 +16,7 @@ class Usuario{
 
 
     function registrarUsuario(){
-        $sql="insert into usuario(nombre,apellido,email,direccion,birthDate,password,estado) values('$this->nombre',$this->apellido,$this->email,$this->direccion,$this->birthDate,$this->password,$this->estado)";
+        $sql="insert into usuario(nombre,apellido,email,direccion,birthDate,password,estado) values('$this->nombre','$this->apellido','$this->email','$this->direccion','$this->birthDate','$this->password',$this->estado)";
         $conexion = new Conexion();
         if($conexion->executeQuery($sql)){
             $conexion->close();
@@ -25,7 +25,7 @@ class Usuario{
     }
 
     function actualizarUsuario(){
-        $sql= "update usuario set nombre='$this->nombre', apellido=$this->apellido, email=$this->email, direccion = $this->direccion, birthDate = $this->birthDate, password = $this->password, estado = '$this->estado' where id =$this->id";
+        $sql= "update usuario set nombre='$this->nombre', apellido='$this->apellido', email='$this->email', direccion = '$this->direccion', birthDate = '$this->birthDate', password = '$this->password', estado = $this->estado where id =$this->id";
         $conexion = new Conexion();
         if($conexion->executeQuery($sql)){
             $conexion->close();

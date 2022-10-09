3cb9f25f03ab0d095048a75e24adacff2e3c194c
Eyder Santiago Suarez <eyder.suarez@correounivalle.edu.co>
2022-10-09 11:30:36 -0500

    Mezclar con cambios de Yuriett

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListadoComponent } from './usuario/listado/listado.component';
-import { UsuarioModule } from './usuario/usuarios.module';
-
 
 const routes: Routes = [
-  {path: '', component:UsuarioModule}
+  {path: '', component:ListadoComponent}
 ];
 
 @NgModule({

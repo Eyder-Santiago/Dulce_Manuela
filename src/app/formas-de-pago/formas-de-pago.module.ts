4c7a96dac81e8b125d553d9c27fb9854a7aaae43
YurietAMM <yuriet.mosquera@correounivalle.edu.co>
2022-10-30 16:40:01 -0500

    pantallas de pagos completas

--- src/app/formas-de-pago/formas-de-pago.module.ts
+++ src/app/formas-de-pago/formas-de-pago.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PagoNequiComponent } from './pago-nequi/pago-nequi.component';
+import { PagoContraEntregaComponent } from './pago-contra-entrega/pago-contra-entrega.component';
 
 
 
 @NgModule({
   declarations: [
-    PagoNequiComponent
+    PagoNequiComponent,
+    PagoContraEntregaComponent
   ],
   imports: [
     CommonModule
